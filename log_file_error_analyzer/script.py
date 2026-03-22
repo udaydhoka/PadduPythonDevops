6b225937e6172bbfd58727a38a34169f6830d901
padmakurba <padma.kurba@gmail.com>
2026-03-22 17:15:28 +0530

    added try except

--- log_file_error_analyzer/script.py
+++ log_file_error_analyzer/script.py
@@ -3,13 +3,19 @@ error_counts = {}
 # take file path from user
 log_file = input("Enter log file path: ")
 
-with open(log_file, "r") as file:
-    for line in file:
-        if "error" in line.lower() or "critical" in line.lower():
-            error = line.strip()
-            error_counts[error] = error_counts.get(error, 0) + 1
+try:
+    with open(log_file, "r") as file:
+        for line in file:
+            if "error" in line.lower() or "critical" in line.lower():
+                error = line.strip()
+                error_counts[error] = error_counts.get(error, 0) + 1
 
-print("\nSummary Report:\n")
 
-for error, count in error_counts.items():
-    print(f"{error} : {count}")
+    print("\nSummary Report:\n")
+
+    for error, count in error_counts.items():
+        print(f"{error} : {count}")
+        
+
+except FileNotFoundError:
+    print("File not found.")
