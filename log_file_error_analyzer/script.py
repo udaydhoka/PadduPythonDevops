b4ac9e551739e54d3a1341873519ce8a6bfcd897
padmakurba <padma.kurba@gmail.com>
2026-03-26 14:59:36 +0530

    updated code

--- log_file_error_analyzer/script.py
+++ log_file_error_analyzer/script.py
@@ -1,24 +1,27 @@
-error_counts = {}
+log_counts = {}
 
 # take file path from user
 log_file = input("Enter log file path: ")
 
+# take log type input
+log_type = input("Enter log type to search (error/warning/info): ").lower()
+
 try:
     with open(log_file, "r") as file:
         for line in file:
-            if "error" in line.lower() or "critical" in line.lower():
-                error = line.strip()
-                error_counts[error] = error_counts.get(error, 0) + 1
+            if log_type in line.lower():
+                log = line.strip()
+                log_counts[log] = log_counts.get(log, 0) + 1
 
+    print(f"\nSummary Report for '{log_type.upper()}' logs:\n")
 
-    print("\nSummary Report:\n")
-
-    for error, count in error_counts.items():
-        print(f"{error} : {count}")
-        
+    if log_counts:
+        for log, count in log_counts.items():
+            print(f"{log} : {count}")
+    else:
+        print(f"No '{log_type}' logs found.")
 
 except FileNotFoundError:
-    print("File not found.")
+    print("Error: File not found. Please check the file path.")
 except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-    raise
+    print(f"Unexpected error occurred: {e}")
