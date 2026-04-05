f6172255f7234c953d61e70794da1909ed31355e
padmakurba <padma.kurba@gmail.com>
2026-04-05 12:35:38 +0530

    updated code

--- server_health_monnitoring_script/script.py
+++ server_health_monnitoring_script/script.py
@@ -1,4 +1,5 @@
 import psutil
+import sys
 
 
 while True:
@@ -14,11 +15,12 @@ while True:
 
     if cpu > 80:
         print("⚠️ CPU is high!")
+        sys.exit()
 
     if memory > 80:
         print("⚠️ Memory is high!")
+        sys.exit()
 
     if disk > 80:
         print("⚠️ Disk is high!")
-
-    
+        sys.exit()
